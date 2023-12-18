a46e28a36d3d09c23afabc7fe137f79652c44a9b
PeterWriteCode <pedrombrancomendes@gmail.com>
2023-12-18 15:08:19 +0000

    objeto done api

--- fetch.js
+++ fetch.js
@@ -1,5 +1,6 @@
 async function fetchProjectsJSON() {
     const response = await fetch('https://api.cosmicjs.com/v3/buckets/deep-strange-web-production/objects?pretty=true&query=%7B%22type%22:%22images%22%7D&limit=10&skip=20&read_key=UaK6ewXkTXExUTsdEZU6sQiFdljWaO4azEp5fE9FfUX7T41VnH&depth=1&props=slug,title,metadata,id,');
+    
     const projects = await response.json();
     return projects.objects;
   }
